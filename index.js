8ec1d2bfc3c2d8f6acd10b8d9bf68f74e940828f
Boruev Isroiljon <140053898+b-isroiljon@users.noreply.github.com>
2024-01-09 09:46:18 +0600

    docs: finish index.js & add 2 files

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function() {
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
 document.body.style.backgroundColor = colors[randomNumber];
 color.textContent = colors[randomNumber];
-// btn.style.backgroundColor = colors[randomNumber];
 });
 
-
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
 
 
 
